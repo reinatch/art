5d169a7eeb8edbf84de5ca5d7f490f75ed0d46d4
reinatch <reinatchnatch@gmail.com>
2024-12-12 09:35:40 +0000

    project detail type

--- src/app/projects/[slug]/page.tsx
+++ src/app/projects/[slug]/page.tsx
@@ -1,34 +1,24 @@
-import ProjectDetailPage from "./ProjectDetailPage";
-import { fetchData } from "@/utils/fetch";
-import { Projecto } from "@/utils/types";
-import { getLocale } from "next-intl/server";
+import ProjectDetailPage from './ProjectDetailPage';
+import { fetchData } from '@/utils/fetch';
+import { Projecto } from '@/utils/types';
+import { getLocale } from 'next-intl/server';
 
-export const revalidate = 60;
+export const revalidate = 60; // Revalidate every 60 seconds
 
 export async function generateStaticParams() {
-  const locales = ["en", "pt"];
-  const allProjects = await Promise.all(
-    locales.map((locale) =>
-      fetchData(
-        `/projectos?acf_format=standard&_fields=id,title,slug&per_page=100&lang=${locale}`
-      )
-    )
-  );
+  const locales = ['en', 'pt']; // Add your supported locales here
+  const allProjects = await Promise.all(locales.map(locale => fetchData(
+    `/projectos?acf_format=standard&_fields=id,title,slug&per_page=100&lang=${locale}`
+  )));
   return allProjects.flat().map((project) => ({
     locale: project.lang,
     slug: project.slug,
   }));
 }
 
-type PageProps = {
-  params: {
-    slug: string;
-  };
-};
-
-export default async function Project({ params }: PageProps) {
-  const { slug } = params;
-  const locale = getLocale();
+export default async function Project({ params }: { params: {slug: string } }) {
+  const { slug } = await params;
+  const locale = await getLocale();
   const fetchProjectData = async () => {
     const allProjects = await fetchData(
       `/projectos?acf_format=standard&_fields=id,title,slug&per_page=100&lang=${locale}`
@@ -43,7 +33,9 @@ export default async function Project({ params }: PageProps) {
 
   const { allProjects, project } = await fetchProjectData();
 
-  const currentIndex = allProjects.findIndex((p: Projecto) => p.slug === slug);
+  const currentIndex = allProjects.findIndex(
+    (p: Projecto) => p.slug === slug
+  );
   const prevProject = currentIndex > 0 ? allProjects[currentIndex - 1] : null;
   const nextProject =
     currentIndex < allProjects.length - 1
